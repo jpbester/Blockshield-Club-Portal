afb3e207b3686f214d3569ebe8fe7da0ae8532fd
Paul <200240+jpbester@users.noreply.github.com>
2022-11-09 20:47:37 +0200

    initial commit

---
+++ Scripts/site.js
@@ -0,0 +1,52 @@
+﻿
+
+function IsEmpty(ElemName) {
+    //  alert(ElemName);
+    //aTextField = document.getElementById(ElemName);
+    //if (aTextField.value.length == 0 || aTextField.value == null) {
+    //    return true;
+    //}
+    //else { return false; }
+
+
+    var textBox = $.trim($(ElemName).val())
+    if (textBox == "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+
+}
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+}
+
+function setElementDisabled(element) {
+    $('body').on('click', element, function () {
+        e.preventDefault();
+        $(this).prop('disabled', true); //disable further clicks
+        return false;
+    });
+}
+
+function unsetDisabledElement(element) {
+    $(element).prop('disabled', false); // re-enable the button once ajax is finished
+}
+
+function setModalElementDisabled(element) {
+    $(element).attr('disabled', true);
+}
+
+
+function unsetModalDisabledElement(element) {
+    $(element).attr('disabled', false); // re-enable the button once ajax is finished
+}
